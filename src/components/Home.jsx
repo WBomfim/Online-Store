344a923a88d1f1bc14e9198cb8206679f662e4ff
Raphael Almeida de Morais <raphaelmoraisdev@gmail.com>
2022-04-26 18:25:40 -0300

    feat:botão pesquisar funcionando

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      productsList: [],
+      searchInput: '',
+    };
+  }
+
+  handleSearch = async () => {
+    const { searchInput } = this.state;
+    const products = await getProductsFromCategoryAndQuery('', searchInput);
+    this.setState({
+      productsList: products.results,
+    });
+  }
+
+  handleChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { searchInput } = this.state;
     return (
       <div>
-        <input type="text" />
-        <button type="button">
+        <input
+          type="text"
+          name="searchInput"
+          value={ searchInput }
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          onClick={ this.handleSearch }
+        >
+          Pesquisar Produto
+        </button>
+        <button
+          type="button"
+        >
           <Link to="/cart" data-testid="shopping-cart-button">
             Carrinho
           </Link>
