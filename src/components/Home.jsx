883389a3852bf9a677bedeb2129a45ce0db45e1e
Raphael Almeida de Morais <raphaelmoraisdev@gmail.com>
2022-04-26 17:54:50 -0300

    feat:botao carrinho adicionado  e rota para o carrinho

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <input type="text" />
+        <button type="button">
+          <Link to="/cart" data-testid="shopping-cart-button">
+            Carrinho
+          </Link>
+        </button>
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
